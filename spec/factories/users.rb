4772235c04906d68b3a5921268f3b19bf0d5c5d4
Diego Hernán Piccinini Lagos <diego@guiasrails.es>
2015-05-11 10:22:57 +0200

    Adds shoulda matchers for user validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    email { FFaker::Internet.email }
+    password "12345678arr"
+    password_confirmation "12345678arr" 
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+  it { should validate_presence_of(:email) }
+	it { should validate_uniqueness_of(:email) }
+	it { should validate_confirmation_of(:password) }
+	it { should allow_value('example@mediacontent.com').for(:email) }
 end
