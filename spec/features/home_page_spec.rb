d577b9ad9c6a203e41c8fde401ed576ba1c561c0
Diego Hernán Piccinini Lagos <diego@guiasrails.es>
2015-05-12 14:00:19 +0200

    Added test and route for search

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -31,4 +31,15 @@ RSpec.feature "HomePage", type: :feature do
     html.should have_selector(:link_or_button, 'Search')
     html.should have_field('text_searched')
   end
+  context "Search a content that not exists" do
+    it "should include a search form " do
+      visit root_path
+      within "form" do
+        fill_in "text_searched", with: "Google Link"
+      end
+      click_link_or_button 'Search' 
+      html.should_not include("Google Link")
+    end
+  end
+
 end
