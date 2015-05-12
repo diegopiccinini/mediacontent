3c7f0047fee74e4df435153d9d1facc2d90dd175
Diego Hernán Piccinini Lagos <diego@guiasrails.es>
2015-05-12 09:36:07 +0200

    Add to the index view test no_published item (reverted from commit 39d10ab28a6aa453dd8c9dcc66d30157c7b456da)

--- spec/views/contents/index.html.erb_spec.rb
+++ spec/views/contents/index.html.erb_spec.rb
@@ -4,15 +4,13 @@ RSpec.describe "contents/index", type: :view do
   before(:each) do
     assign(:contents, [
       Content.create!(FactoryGirl.attributes_for(:content)),
-      Content.create!(FactoryGirl.attributes_for(:content_video)),
-      Content.create!(FactoryGirl.attributes_for(:not_published))
+      Content.create!(FactoryGirl.attributes_for(:content_video))
     ])
   end
 
   it "renders a list of contents" do
     content=FactoryGirl.create(:content) 
     content_video=FactoryGirl.create(:content_video) 
-    conten_not_published =Content.create!(FactoryGirl.attributes_for(:not_published))
 
     render
     assert_select "tr>td", :text => content.name.to_s, :count => 1
@@ -20,6 +18,6 @@ RSpec.describe "contents/index", type: :view do
     assert_select "tr>td", :text => content.content_type.to_s, :count => 1
 
     assert_select "tr>td", :text => content_video.content.to_s, :count => 1
-    assert_select "tr>td", :text => conten_not_published.content.to_s, :count => 0
+
   end
 end
