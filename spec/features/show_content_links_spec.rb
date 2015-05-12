8ec9aa9a69b2e514548879cf56d351e4fcf573b8
Diego Hernán Piccinini Lagos <diego@guiasrails.es>
2015-05-12 11:51:08 +0200

    Add test to check iframe link

--- spec/features/show_content_links_spec.rb
+++ spec/features/show_content_links_spec.rb
@@ -20,21 +20,40 @@ RSpec.feature "ShowContentLinks", type: :feature do
 
     click_link_or_button "Create Content"
 
-    visit content_path
-    html.should include("Video MongoDB")
-    html.should have_css("a", :text => "Show")
+    visit content_path(Content.first.slug)
+    html.should include("Video MongoDB") 
     html.should have_css("a", :text => "Edit")
     html.should have_css("a", :text => "Back")
 
     logout(:user)
-    visit content_path
- 
-    html.should include("Video MongoDB")
-    html.should have_css("a", :text => "Show")
+    visit content_path(Content.first.slug)
+    html.should include("Video MongoDB") 
     html.should_not have_css("a", :text => "Edit")
     html.should have_css("a", :text => "Back")
     
   end
+  it "should have an iframe content" do
 
+    visit new_content_path
+
+
+    within "#new_content" do
+      fill_in "content_name", with: "Video MongoDB"
+      find('select#content_content_type').find("option[value='video']").select_option     
+      fill_in "content_content", with: "<iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/rRpSgv1JyOo\" frameborder=\"0\" allowfullscreen></iframe>"
+      check "content_published"
+      
+    end
+
+    click_link_or_button "Create Content"
+
+
+    logout(:user)
+    visit content_path(Content.first.slug)
+    html.should include("Video MongoDB") 
+    html.should include("<iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/rRpSgv1JyOo\" frameborder=\"0\" allowfullscreen></iframe>") 
+
+    
+  end
 
 end
